319a88d03f331f265d9307885e8b6aff9c1aa4d0
dhruvbhartia07 <dhruvbhartia07@gmail.com>
2022-03-10 09:48:55 +0530

    2 files modified

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-require('dotenv').config()
+require('dotenv').config();
 const mongoose = require('mongoose');
-const express = require("express");
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const authRoutes = require("./routes/auth");
+const authRoutes = require('./routes/auth');
 
 //DB Connection
-mongoose.connect(process.env.DATABASE)
-  .then(() => {
-  console.log("DB CONNECTED");
-  });
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log('DB CONNECTED..');
+});
 
 //Middlewares
 app.use(bodyParser.json());
@@ -20,11 +19,10 @@ app.use(cookieParser());
 app.use(cors());
 
 //My Routes
-app.use("/api", authRoutes);
+app.use('/api', authRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
-
 
 //Starting a server
 app.listen(port, () => {
